3cee1d53d613c3d436e34bccf5ce39594d228963
root <andrerrcosta@gmail.com>
2021-02-23 14:03:10 -0300

    removing double cors

--- zanalyticsclient/src/main/java/com/nobblecrafts/zanalyticsclient/configuration/WebConfiguration.java
+++ zanalyticsclient/src/main/java/com/nobblecrafts/zanalyticsclient/configuration/WebConfiguration.java
@@ -8,10 +8,10 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @Configuration
 @EnableWebFlux
 public class WebConfiguration implements WebFluxConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200")
-            .allowedMethods("PUT", "POST", "DELETE", "GET");
-    }
+    // @Override
+    // public void addCorsMappings(CorsRegistry corsRegistry) {
+    //     corsRegistry.addMapping("/**")
+    //         .allowedOrigins("http://localhost:4200")
+    //         .allowedMethods("PUT", "POST", "DELETE", "GET");
+    // }
 }
--- zwavestream/src/main/java/com/nobblecrafts/zwavestream/configuration/WebConfiguration.java
+++ zwavestream/src/main/java/com/nobblecrafts/zwavestream/configuration/WebConfiguration.java
@@ -9,11 +9,11 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @EnableWebFlux
 public class WebConfiguration implements WebFluxConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200")
-            .allowedMethods("PUT", "POST", "DELETE", "GET");
-    }
+    // @Override
+    // public void addCorsMappings(CorsRegistry corsRegistry) {
+    //     corsRegistry.addMapping("/**")
+    //         .allowedOrigins("http://localhost:4200")
+    //         .allowedMethods("PUT", "POST", "DELETE", "GET");
+    // }
 
 }
